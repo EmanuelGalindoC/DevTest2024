2cd0b79f55be591dd3f2f3ddbb317ae93f884831
Emanuel Galindo <emanuel.galindo.9362056@usalesiana.edu.bo>
2024-11-09 11:51:42 -0400

    feat : Adding frontendserver

---
+++ testfrontend/src/ApiService/ApiService.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const API_URL ='http://localhost:8082/api/polls';
+
+export const getPolls = async() => {
+    
+}
---
+++ testfrontend/src/Components/AddPoll/AddPoll.js
@@ -0,0 +1,16 @@
+import React,{useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const AddPoll = () => {
+    const [name, setName] = useState();
+
+  return (
+    <div>AddPoll
+        <form>
+            
+        </form>
+    </div>
+  )
+}
+
+export default AddPoll
